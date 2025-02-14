0dc2b48389cc5ddca651a167f9a3cda0f6af0040
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-02-14 11:50:59 +0530

    Adding chat history

--- langchain-web.py
+++ langchain-web.py
@@ -3,28 +3,30 @@ load_dotenv()
 
 import os
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.document_loaders import PDFBaseLoader
-from langchain.chains.question_answering import load_qa_chain
-from langchain_core.prompts import PromptTemplate
-from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_google_genai import GoogleGenerativeAI
-
-from typing_extensions import TypedDict, List
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 from pinecone import Pinecone as pc
 
-class State(TypedDict):
-    question: str
-    context: List[Document]
-    answer: str
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 pinecone_client = pc(api_key = PINECONE_API_KEY)
+def pinecone_init(embedding_model, host):
+    index = pinecone_client.Index(host = host)
+    vectorstore = PineconeVectorStore(embedding = embedding_model, index = index)
+    return vectorstore
 
 def load_website(vectorstore, url: str):
     loader = WebBaseLoader(web_paths = [url])
@@ -34,16 +36,13 @@ def load_website(vectorstore, url: str):
     vectorstore.add_documents(splits)
 
 def load_pdf(vectorstore, path: str):
-    loader = PDFBaseLoader(pdf_paths = [path])
+    loader = PyPDFLoader(pdf_paths = [path])
     docs = loader.load()
     text_spliiter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap=50)
     splits = text_spliiter.split_documents(docs)
     vectorstore.add_documents(splits)
 
-def pinecone_init(embedding_model, host):
-    index = pinecone_client.Index(host = host)
-    vectorstore = PineconeVectorStore(embedding = embedding_model, index = index)
-    return vectorstore
+
 
     
 
@@ -55,21 +54,43 @@ if __name__ == "__main__":
 
     llm = GoogleGenerativeAI(model=llm_model, temperature=0.7, top_p=0.85)
     gemini_embeddings = GoogleGenerativeAIEmbeddings(model = embedding_model)
+    vectorstore = pinecone_init(gemini_embeddings, pinecone_host)
+    store_retriever = vectorstore.as_retriever()
 
-    prompt_template = """You are an assistant for question answering tasks.
+    contextual_system_prompt = """Given a chat history and the latest user query which might 
+    reference context in the history, generate a standalone question which can be understood without
+    the chat history. Do NOT answer the question, just generate it if needed otherwise return it
+    as is."""
+
+    contextual_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextual_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
+
+    histry_aware_retriever = create_history_aware_retriever(llm, store_retriever, contextual_prompt)
+
+    system_prompt = """You are an assistant for question answering tasks.
     Use only the given context to answer the question.
     If you don't know the answer just say I don't know.
     Use a maximum of 3 sentences to answer the question.
 
-    Question: {input}
     Context: {context}
-    Answer: """
+    """
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
 
-    PROMPT = PromptTemplate.from_template(prompt_template)
+    qa_chain = create_stuff_documents_chain(llm , prompt)
+    rag_chain = create_retrieval_chain(histry_aware_retriever, qa_chain)
+    
 
-    vectorstore = pinecone_init(gemini_embeddings, pinecone_host)
-    retriever = vectorstore.as_retriever()
-    chain = load_qa_chain(llm, chain_type="stuff")
 
     while True:
         question = input("Enter your question: ")
@@ -82,8 +103,7 @@ if __name__ == "__main__":
         elif question.lower() == "web":
             url = input("Enter the url: ")
             docs = load_website(vectorstore, url)
-        docs = retriever.invoke(question)
         response = chain.invoke({"input_documents": docs, "question": question})
-        print(response["answer"])
+        print(response)
 
 
